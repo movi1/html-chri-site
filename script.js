d3e10a7f42e5c2d90357b3f0e2056f8a78a36704
movi1 <98534830+movi1@users.noreply.github.com>
2023-06-27 15:00:09 +0100

    add some images and navbar

---
+++ script.js
@@ -0,0 +1,27 @@
+// const toggleMenu = () => {
+//     document.body.classList.toggle("open");
+// };
+// const homepageLink = document.getElementById('homepage-link');
+// const portfolioLink = document.getElementById('portfolio-link');
+// const printLink = document.getElementById('print-link');
+// const aboutLink = document.getElementById('about-link');
+
+// homepageLink.addEventListener('click', navigateToPage);
+// portfolioLink.addEventListener('click', navigateToPage);
+// printLink.addEventListener('click', navigateToPage);
+// aboutLink.addEventListener('click', navigateToPage);
+
+// function navigateToPage(event) {
+//   event.preventDefault();
+//   const pageUrl = event.target.getAttribute('href');
+//   window.location.href = pageUrl;
+// }
+
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('.navbar');
+//     if (window.pageYOffset > 0) {
+//       navbar.classList.add('sticky');
+//     } else {
+//       navbar.classList.remove('sticky');
+//     }
+//   });
